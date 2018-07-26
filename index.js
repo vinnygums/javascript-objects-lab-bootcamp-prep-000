4103c76ff6e93acea8510072651f55876239bb22
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-26 19:13:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
   
-  
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
