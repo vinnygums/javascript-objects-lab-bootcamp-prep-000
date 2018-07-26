28213899ab3e6bd9ccef1755b7506d5e11d1c67e
Vicente Soriano <vicente.soriano@icloud.com>
2018-07-26 19:03:45 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object, { [key]: value} )
+  return newObj
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
